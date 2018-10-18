5fb8e11f4a7d0571d88bbf4015de66564edb2248
Alexander Steen <alexander.steen@academicwork.se>
2018-10-18 11:45:51 +0200

    Added GET endpoint

--- src/main/java/bjj/controller/TestController.java
+++ src/main/java/bjj/controller/TestController.java
@@ -3,6 +3,7 @@ package bjj.controller;
 import bjj.database.DatabaseConnection;
 import bjj.domain.TrainingSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,12 @@ import java.util.List;
 public class TestController {
 
     @RequestMapping("/")
-    public List<TrainingSession> index() {
+    public String index() {
+        return "App is running!";
+    }
+
+    @GetMapping("/trainingsessions")
+    public List<TrainingSession> getTrainingSessions() {
         DatabaseConnection connection = new DatabaseConnection();
         return connection.getAllTrainingSessions();
     }
