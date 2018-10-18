3da64bac2039e90c68f259295246eaab854d2fca
Alexander Steen <alexander.steen@academicwork.se>
2018-10-18 12:12:19 +0200

    Added xml and CORS

--- src/main/java/bjj/Application.java
+++ src/main/java/bjj/Application.java
@@ -2,12 +2,14 @@ package bjj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+
     }
 
 }
