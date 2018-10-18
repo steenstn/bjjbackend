2457bc8a0012e9d2a5cfb780b90a2a562d4108b5
Alexander Steen <alexander.steen@academicwork.se>
2018-10-18 12:17:23 +0200

    Cross origin annotation

--- src/main/java/bjj/controller/TestController.java
+++ src/main/java/bjj/controller/TestController.java
@@ -3,6 +3,7 @@ package bjj.controller;
 import bjj.database.DatabaseConnection;
 import bjj.domain.TrainingSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class TestController {
 
     @RequestMapping("/")
