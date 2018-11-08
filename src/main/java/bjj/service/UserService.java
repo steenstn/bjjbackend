c1fbfa117e03519debc1b91958751f03550416f1
Alexander Steen <alexander.steen@academicwork.se>
2018-11-08 11:22:14 +0100

    Added missing files

---
+++ src/main/java/bjj/service/UserService.java
@@ -0,0 +1,8 @@
+package bjj.service;
+
+import bjj.domain.User;
+
+public interface UserService {
+    User registerUser(String username, String password);
+    String login(String username, String password);
+}
---
+++ src/main/java/bjj/service/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package bjj.service;
+
+import bjj.database.UserRepository;
+import bjj.domain.Role;
+import bjj.domain.User;
+import bjj.security.JwtTokenProvider;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+@Component
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private JwtTokenProvider jwtTokenProvider;
+
+    public UserServiceImpl(UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
+        this.userRepository = userRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public User registerUser(String username, String password) {
+        if(userRepository.userExists(username)) {
+            throw new RuntimeException("User with username '" + username + "' already exists");
+        }
+        return userRepository.registerUser(username, password);
+    }
+
+    @Override
+    public String login(String username, String password) {
+        User dbUser = userRepository.getUser(username);
+        if(BCrypt.checkpw(password, dbUser.getPassword())) {
+            return jwtTokenProvider.createToken(dbUser.getUsername(), new ArrayList<>(Collections.singleton(Role.ROLE_USER)));
+        } else {
+            throw new RuntimeException("Unsuccessful login");
+        }
+    }
+}
